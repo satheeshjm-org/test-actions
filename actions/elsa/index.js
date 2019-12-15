21f900965c4d54292302de90593719354f26819e
jm <satheesh.jm@freshworks.com>
2019-12-15 21:51:56 +0530

    elsa

--- actions/elsa/index.js
+++ actions/elsa/index.js
@@ -6,20 +6,24 @@ var Commander = require ("../../_shared/commander")
 
 async function run() {
 
-  const context = github.context
+  try {
+    const context = github.context
 
-  //shared libraries were implemented for probot, so this is needed for compatibility
-  const github_cli = new github.GitHub(process.env.GITHUB_TOKEN)
-  context.github = github_cli
+    //shared libraries were implemented for probot, so this is needed for compatibility
+    const github_cli = new github.GitHub(process.env.GITHUB_TOKEN)
+    context.github = github_cli
 
+    const FreezeCommand = require("./commands/freeze_branch.js")
+    const freezeCommand = new FreezeCommand(context)
+    await (new Commander.Commander(freezeCommand).execute())
 
-  const FreezeCommand = require ("./commands/freeze_branch.js")
-  const freezeCommand = new FreezeCommand(context)
-  await (new Commander.Commander(freezeCommand).execute())
-
-  var UnfreezeCommand = require ("./commands/unfreeze_branch.js")
-  var unfreezeCommand = new UnfreezeCommand(context)
-  await (new Commander.Commander(unfreezeCommand).execute())
+    var UnfreezeCommand = require("./commands/unfreeze_branch.js")
+    var unfreezeCommand = new UnfreezeCommand(context)
+    await (new Commander.Commander(unfreezeCommand).execute())
+  }
+  catch (e) {
+    core.setFailed(e.message);
+  }
 }
 
 
