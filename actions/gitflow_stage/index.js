da71a9a9578145059f526228a26278a79ff80b5e
jm <satheesh.jm@freshworks.com>
2019-12-24 10:05:42 +0530

    x

--- actions/gitflow_stage/index.js
+++ actions/gitflow_stage/index.js
@@ -3,6 +3,106 @@ const github = require('@actions/github');
 const parse_diff = require('parse-diff');
 const minimatch = require("minimatch")
 
+
+async function construct_pr_body(staging_branch, production_branch, body_config) {
+
+  var table_fields = body_config.table_fields || []
+
+  //table title
+  var titles = []
+  //line separating title and contents
+  var horizontal_linesegments = []
+  //table rows
+  var table_rows = []
+
+  for (var j=0;j<table_fields.length;j++) {
+    titles.push(table_fields[j].name)
+    horizontal_linesegments.push("---")
+  }
+
+  //find diff commits
+  const comparecommits_resp = await github_cli.repos.compareCommits({
+    owner: repo.owner,
+    repo: repo.repo,
+    base: production_branch,
+    head: staging_branch
+  })
+
+  const commits = comparecommits_resp.data.commits
+  for (var j=0; j<commits.length; j++) {
+
+    var table_row = {}
+    table_rows.push(table_row)
+
+
+    //calculate filenames changed in commit
+    const commit = commits[j]
+    const author = commit.author.login
+    const message = commit.commit.message
+
+    var commitresp = await github_cli.repos.getCommit({
+      owner: repo.owner,
+      repo: repo.repo,
+      ref: resp.data.commits[0].sha
+    })
+
+    var files = commitresp.data.files
+    var filenames_changed = files.map(f => f.filename)
+
+
+    for (var j=0;j<table_fields.length;j++) {
+      var table_field = table_fields[j]
+
+
+      var value = table_field.value
+      if (value == "pr") {
+        table_row.push(`${message})`)
+      }
+      else if (value == "owner") {
+        table_row.push(`@${author}`)
+      }
+      else if (value == "does_file_exist") {
+        var glob_to_match = table_field.glob
+        console.debug(`Glob to match : ${glob_to_match}`)
+
+
+        console.log(`${filenames_changed.length} Files found in PR : ${filenames_changed}`)
+
+        var glob_match = null
+        for(var k=0; k< filenames_changed.length; k++) {
+          const f = filenames_changed[k]
+          if(minimatch(f, glob_to_match)) {
+            glob_match = f
+            break
+          }
+        }
+
+        if (glob_match) {
+          console.debug(`matching file ${glob_match} found for glob ${glob_to_match} found in PR diff`)
+          table_row.push(`<ul><li>- [x] </li></ul>`)
+        }
+        else {
+          console.debug(`no matching file found in PR diff for glob ${glob_to_match}`)
+          table_row.push(`<ul><li>- [ ] </li></ul>`)
+        }
+      }
+    }
+  }
+
+
+
+
+  var pr_body = `| ${titles.join(" | ")} |\n`
+  pr_body = `${pr_body}| ${horizontal_linesegments.join(" | ")} |\n`
+  table_rows.forEach(tr => {
+    pr_body = `${pr_body}| ${tr.join(" | ")} |\n`
+  })
+}
+
+
+
+
+
 async function run() {
 
   try {
@@ -14,104 +114,22 @@ async function run() {
     const staging_branch = core.getInput('staging_branch')
     const production_branch = core.getInput('production_branch')
     const body_config_str = core.getInput('pr_body_config') || '{}'
-    console.log(body_config_str)
     const body_config = JSON.parse(body_config_str)
     console.table(body_config)
 
-    var table_fields = body_config.table_fields || []
 
     const payload = context.payload
     var repo = context.repo
 
-    var payload_pr = payload.pull_request
-    if(!payload_pr) {
-      throw new Error(`Payload "pull_request" missing`)
-    }
-    var is_merged = payload_pr.merged
-    if (!is_merged) {
-      console.info(`This is not a merge`)
-      return ;
-    }
-
-    /**
-     * allowed PRs : (!master) -> dev
-     *  1. if base_branch is not staging, dont run (This won't be needed, if you have proper conditions in the workflow config)
-     *  2. if head_branc is master, don't run (this is just a reverse merge)
-     */
-    if(payload_pr.base.ref != staging_branch) {
-      console.info(`Require base:${production_branch} for creating a release`)
-    }
-    if(payload_pr.head.ref == production_branch) {
-      console.info(`Require head:${staging_branch} for creating a release`)
-      return
-    }
-
-
     var base = production_branch //pull from config
-    var head = payload_pr.base.ref
+    var head = staging_branch
     var log_prefix = `${head}->${base}`
 
-
-    var titles = []
-    var horizontal_linesegments = []
-    var current_pr_entry = []
-    for (var i=0;i<table_fields.length;i++) {
-      var table_field = table_fields[i]
-
-      var name = table_field.name
-      titles.push(name)
-      horizontal_linesegments.push("---")
-
-      var value = table_field.value
-      if (value == "pr") {
-        current_pr_entry.push(`${payload_pr.title}(#${payload_pr.number})`)
-      }
-      else if (value == "owner") {
-        current_pr_entry.push(`@${payload_pr.user.login}`)
-      }
-      else if (value == "does_file_exist") {
-        var glob_to_match = table_field.glob
-        console.debug(`Glob to match : ${glob_to_match}`)
-
-        //get diff
-        var prget_resp = await github_cli.pullRequests.get({
-          owner: repo.owner,
-          repo: repo.repo,
-          pull_number: payload_pr.number,
-          headers: {accept: "application/vnd.github.v3.diff"}
-        });
-
-        var diffs = parse_diff(prget_resp.data)
-        var diff_files = diffs.map(d => d.to)
-
-        console.log(`Files found in PR ${diff_files}`)
-
-
-        var glob_match = null
-        for(var j=0; j< diff_files.length; j++) {
-          const f = diff_files[j]
-          if(minimatch(f, glob_to_match)) {
-            glob_match = f
-            break
-          }
-        }
-
-        if (glob_match) {
-          console.debug(`matching file ${glob_match} found for glob ${glob_to_match} found in PR diff`)
-          current_pr_entry.push(`<ul><li>- [x] </li></ul>`)
-        }
-        else {
-          console.debug(`no matching file found in PR diff for glob ${glob_to_match}`)
-          current_pr_entry.push(`<ul><li>- [ ] </li></ul>`)
-        }
-      }
-    }
-
-
-    var pr_body = `| ${current_pr_entry.join(" | ")} |`
-
+    var pr_body = construct_pr_body(staging_branch, production_branch, body_config)
 
     try {
+
+      //check if a pull request already exists
       console.debug(`${log_prefix} Fetching pull request`)
       var prlist_resp = await github_cli.pulls.list({
         owner: repo.owner,
@@ -123,10 +141,9 @@ async function run() {
 
       var prs = prlist_resp.data
       if (prs.length == 0) {
+        //no PRs are found. so create one
         console.info(`${log_prefix} Pull request not found. So creating one`)
 
-        pr_body = `| ${horizontal_linesegments.join(" | ")} |\n${pr_body}`
-        pr_body = `| ${titles.join(" | ")} |\n${pr_body}`
         var prcreate_resp = await github_cli.pulls.create({
           owner: repo.owner,
           repo: repo.repo,
@@ -138,10 +155,14 @@ async function run() {
         console.info(`${log_prefix} Pull request created`)
       }
       else {
+        //PR is found. update the body of the PR by appending the current PR entry
         var existing_pr = prs[0]
 
         console.info(`${log_prefix} ${prs.length} pull requests found. ${existing_pr.number}`)
-        pr_body = `${existing_pr.body}\n${pr_body}`
+
+        var existing_pr_body = existing_pr.body
+        existing_pr_body.replace(`|.*|`,"")
+        pr_body = `${existing_pr.body}\n${pr_body}\n`
 
         var prupdate_resp = await github_cli.pulls.update({
           owner: repo.owner,
@@ -149,7 +170,6 @@ async function run() {
           pull_number: prs[0].number,
           body: pr_body
         })
-        //update description
       }
     }
     catch (e) {
